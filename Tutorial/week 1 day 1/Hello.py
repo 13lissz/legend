6d6ce131f6835334608d686afbeb1ed2da83ebe9
13lissz <nicoempin03@gmail.com>
2023-02-13 13:42:06 +0800

    Percubaan Pertama

---
+++ Tutorial/week 1 day 1/Hello.py
@@ -0,0 +1,6 @@
+name = input("Enter Your name : ")
+studId = input("Enter student id : ")
+celcius = float(input("Enter your temparature"))
+temp = (celcius*1.8)+32
+
+print("Your temperature is",temp)
